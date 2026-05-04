63a196bbfc4548ae02e3a0037afe1568d581747d
User Name <user@example.com>
2026-05-04 14:47:52 -0400

    Remove .venv and logs from tracking, expand .gitignore

--- .venv/lib/python3.12/site-packages/sec_downloader/__init__.py
+++
@@ -1,5 +0,0 @@
-__version__ = "0.12.2"
-from sec_downloader.core import Downloader
-from sec_downloader.download_storage import DownloadStorage, FileContent
-
-__all__ = ["Downloader", "DownloadStorage", "FileContent"]
